7eb7e5aab4bab021fa41c06f1a7e51bfc2327948
Dan <jdanray@users.noreply.github.com>
2018-10-20 16:21:00 -0500

    push complexNumberMultiply.py

---
+++ complexNumberMultiply.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/complex-number-multiplication/description/
+
+class Solution:
+	def complexNumberMultiply(self, A, B):
+		def parse(M):
+			i = M.find('+')
+			j = M.find('i')
+
+			a = int(M[:i])
+			b = int(M[i + 1:j])
+
+			return (a, b)
+
+		def mult(M, N):
+			a, b = M
+			c, d = N
+
+			e = (a * c - b * d)
+			f = (b * c + a * d)
+
+			return (e, f)
+
+		def compose(e, f):
+			return str(e) + "+" + str(f) + "i"
+
+		return compose(*mult(parse(A), parse(B)))
