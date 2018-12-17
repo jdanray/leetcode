7dd97e16bd670dffc860c3dca1ac59e82b0cf144
Dan <jdanray@users.noreply.github.com>
2018-12-16 21:07:41 -0600

    added binary-search soln to peakIndexInMountainArray

--- peakIndexInMountainArray.py
+++ peakIndexInMountainArray.py
@@ -6,3 +6,16 @@ class Solution(object):
 		while A[i] < A[i + 1]:
 			i += 1
 		return i
+
+class Solution(object):
+	def peakIndexInMountainArray(self, A):
+		lo = 0
+		hi = len(A) - 1
+		while lo < hi:
+			mid = lo + (hi - lo) // 2
+			if A[mid - 1] > A[mid]:
+				hi = mid
+			elif A[mid] < A[mid + 1]:
+				lo = mid
+			else:
+				return mid
