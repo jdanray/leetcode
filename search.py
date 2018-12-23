5e36dac09105f3bb999ad71bb0de1b91a6c3c562
Dan <jdanray@users.noreply.github.com>
2018-12-23 16:09:43 -0600

    push search.py

---
+++ search.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/binary-search
+
+class Solution(object):
+	def search(self, nums, target):
+		lo = 0
+		hi = len(nums) - 1
+		while lo <= hi:
+			mid = lo + (hi - lo) // 2
+			if nums[mid] == target:
+				return mid
+			elif nums[mid] < target:
+				lo = mid + 1
+			elif nums[mid] > target:
+				hi = mid - 1
+		return -1
