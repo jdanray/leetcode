8975b577df717a3ff03635df78f0e1716cfc1dd7
Dan <jdanray@users.noreply.github.com>
2019-01-09 20:54:10 -0600

    add URL to calculate.py; push calculate2.py

--- calculate2.py
+++ calculate2.py
@@ -1,16 +1,27 @@
-# https://leetcode.com/problems/basic-calculator/
+# https://leetcode.com/problems/basic-calculator-ii/
 
 class Solution(object):
 	def calculate(self, s):
 		values = []
 		operators = []		
 		i = 0
+
+		# PEMDAS
+		precedence = {}
+		precedence['*'] = 2
+		precedence['/'] = 2
+		precedence['+'] = 1
+		precedence['-'] = 1
 		
 		def operate(op, right, left):
 			if op == '+':
 				return left + right
 			elif op == '-':
 				return left - right
+			elif op == '*':
+				return left * right
+			elif op == '/':
+				return left // right
 		
 		while i < len(s):
 			if s[i].isdigit():
@@ -20,17 +31,10 @@ class Solution(object):
 					i += 1
 				values.append(int(num))
 				i -= 1
-			elif s[i] in {'+', '-'}:
-				while operators and operators[-1] != '(':
+			elif s[i] in {'+', '-', '*', '/'}:
+				while operators and precedence[operators[-1]] >= precedence[s[i]]:
 					values.append(operate(operators.pop(), values.pop(), values.pop()))
 				operators.append(s[i])
-			elif s[i] == '(':
-				operators.append(s[i])
-			elif s[i] == ')':
-				while operators[-1] != '(':
-					values.append(operate(operators.pop(), values.pop(), values.pop()))
-				operators.pop()
-			
 			i += 1
 
 		while operators:
