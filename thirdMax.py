8c491d4cafb14c7a6ba8da85b10d0d634b987ea2
Dan <jdanray@users.noreply.github.com>
2019-01-12 23:31:12 -0600

    add O(1)-space soln to thirdMax.py

--- thirdMax.py
+++ thirdMax.py
@@ -1,5 +1,27 @@
 # https://leetcode.com/problems/third-maximum-number/
 
+# https://leetcode.com/problems/third-maximum-number/
+
+class Solution(object):
+	def thirdMax(self, nums):
+		max1 = -sys.maxsize
+		max2 = -sys.maxsize
+		max3 = -sys.maxsize
+		for n in nums:
+			if n == max1 or n == max2 or n == max3:
+				continue
+			elif n > max1:
+				max3 = max2
+				max2 = max1
+				max1 = n
+			elif n > max2:
+				max3 = max2
+				max2 = n
+			elif n > max3:
+				max3 = n
+                
+		return max1 if max3 == -sys.maxsize else max3
+
 class Solution(object):
 	def thirdMax(self, nums):
 		max1 = -sys.maxsize
