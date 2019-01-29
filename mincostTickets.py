3daa77378326a87dbe1fbadce8e45c73a999d369
Dan <jdanray@users.noreply.github.com>
2019-01-28 22:41:55 -0600

    add another soln to mincostTickets.py

--- mincostTickets.py
+++ mincostTickets.py
@@ -21,3 +21,22 @@ class Solution(object):
 			return memo[d]
 
 		return mincost(0)
+
+# After I solve a problem, I like to study other people's solutions to the same problem.
+# The following is the top-voted solution, translated from Java into Python.
+# Link to original: https://leetcode.com/problems/minimum-cost-for-tickets/discuss/226670/Java-DP-Solution-with-explanation-O(n)
+
+class Solution(object):
+	def mincostTickets(self, days, costs):
+		days = set(days)
+		N = 365
+		mincost = [0 for _ in range(N + 1)]
+		for d in range(1, N + 1):
+			if d not in days:
+				mincost[d] = mincost[d - 1]
+			else:
+				a = costs[0] + mincost[d - 1]
+				b = costs[1] + mincost[max(d - 7, 0)]
+				c = costs[2] + mincost[max(d - 30, 0)]
+				mincost[d] = min(a, b, c)
+		return mincost[365]
