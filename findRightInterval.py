1d5eb32de352e42ea78db7812510e2c4118357ff
Dan <jdanray@users.noreply.github.com>
2019-03-07 23:20:18 -0600

    replace findRightInterval.py with simpler soln

--- findRightInterval.py
+++ findRightInterval.py
@@ -2,23 +2,17 @@
 
 class Solution:
 	def findRightInterval(self, intervals):
-		sortivls = sorted(enumerate(intervals), key=lambda ivl: ivl[1].start)
-		res = [-1 for _ in range(len(intervals))]
-		for j in range(len(sortivls)):
-			i, ivl = sortivls[j]
-			idx = self.search(sortivls, ivl, j + 1)
-			res[i] = idx
-		return res
+		start = sorted(range(len(intervals)), key=lambda i: intervals[i].start)
+		finish = sorted(range(len(intervals)), key=lambda i: intervals[i].end)
 
-	def search(self, sortivls, ivl, lo):
-		idx = -1
-		hi = len(sortivls) - 1
-		while lo <= hi:
-			mid = lo + (hi - lo) // 2
-			i, sivl = sortivls[mid]
-			if sivl.start < ivl.end:
-				lo = mid + 1
+		i = 0
+		j = 0
+		right = [-1] * len(intervals)
+		while i < len(finish) and j < len(start):
+			if intervals[finish[i]].end <= intervals[start[j]].start:
+				right[finish[i]] = start[j]
+				i += 1
 			else:
-				hi = mid - 1
-				idx = i
-		return idx
+				j += 1
+
+		return right
