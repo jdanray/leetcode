1d76d3bb1793e59cc180a9fd3a3f093b1071d0e5
Dan <jdanray@users.noreply.github.com>
2019-03-09 21:13:09 -0600

    push minPathSum.py

---
+++ minPathSum.py
@@ -0,0 +1,51 @@
+# https://leetcode.com/problems/minimum-path-sum/
+
+class Solution:
+	def minPathSum(self, grid):
+		if not grid:
+			return 0
+
+		M = len(grid)
+		N = len(grid[0])
+
+		memo = {}
+		def helper(i, j):
+			if (i, j) in memo:
+				return memo[(i, j)]
+
+			m = grid[i][j]
+			k = i + 1
+			l = j + 1
+
+			if k < M and l < N:
+				m += min(helper(k, j), helper(i, l))
+			elif k < M:
+				m += helper(k, j)
+			elif l < N:
+				m += helper(i, l)
+
+			memo[(i, j)] = m
+			return memo[(i, j)]
+
+		return helper(0, 0)
+
+class Solution(object):
+	memo = {}
+
+	def minPathSum(self, grid):
+		self.memo = {}
+		return self.RminPathSum(grid, 0, 0)
+
+	def RminPathSum(self, grid, i, j):
+		m = len(grid)
+		n = len(grid[0])
+		if i == m - 1 and j == n - 1:
+			return grid[i][j]
+		if (i, j) not in self.memo:
+			dirs = []
+			if i + 1 < m:
+				dirs.append((i + 1, j))
+			if j + 1 < n:
+				dirs.append((i, j + 1))
+			self.memo[(i, j)] = grid[i][j] + min(self.RminPathSum(grid, *d) for d in dirs)
+		return self.memo[(i, j)]
