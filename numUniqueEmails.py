55f24cc01f6aa69da55e59b83f6db15bcabe073a
Dan <jdanray@users.noreply.github.com>
2019-03-17 01:37:06 -0500

    push numUniqueEmails.py

---
+++ numUniqueEmails.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/interview/1/
+
+class Solution(object):
+	def numUniqueEmails(self, emails):
+		unique = set()
+		for mail in emails:
+			i = 0
+			local = ''
+			plus = False
+			while mail[i] != "@":
+				if mail[i] == '.':
+					i += 1
+				elif mail[i] == '+' or plus:
+					plus = True
+					i += 1
+				else:
+					local += mail[i]
+					i += 1
+			unique.add(local + mail[i:])
+		return len(unique)
