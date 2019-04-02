823960799a90b25b6406e62d6e66201c319ec6f9
Dan <jdanray@users.noreply.github.com>
2019-04-02 15:02:29 -0500

    edited nextGreaterElements.java for clarity

--- nextGreaterElements.java
+++ nextGreaterElements.java
@@ -1,20 +1,22 @@
 // https://leetcode.com/problems/next-greater-element-ii/description/
 
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int j = i;
-            do {
-                j++;
-                if (j >= nums.length)
-                    j = 0;
-            } while (nums[j] <= nums[i] && j != i);
-            if (j == i)
-                res[i] = -1;
-            else
-                res[i] = nums[j];
-        }
-        return res;        
-    }
+	public int[] nextGreaterElements(int[] nums) {
+		int[] res = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+            		int j = i;
+			do {
+				j++;
+				if (j >= nums.length)
+					j = 0;
+			} while (nums[j] <= nums[i] && j != i);
+
+			if (j == i)
+				res[i] = -1;
+			else
+				res[i] = nums[j];
+		}
+
+		return res;
+	}
 }
