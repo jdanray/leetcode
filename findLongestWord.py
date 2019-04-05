468cd3604465a797c20104885403155085bcdbc9
Dan <jdanray@users.noreply.github.com>
2019-04-05 14:16:20 -0500

    added soln to findLongestWord.py

--- findLongestWord.py
+++ findLongestWord.py
@@ -1,9 +1,10 @@
 # https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
 
-class Solution(object):
+class Solution:
 	def findLongestWord(self, s, d):
+		d = sorted(d)
 		longest = ''
-		for w in sorted(d):
+		for w in d:
 			if len(w) < len(longest) or len(w) > len(s):
 				continue
 
@@ -13,6 +14,26 @@ class Solution(object):
 					i += 1
 
 			if i == len(w) and len(w) > len(longest):
+				longest = w
+
+		return longest
+
+class Solution:
+	def findLongestWord(self, s, d):
+		longest = ''
+		for w in d:
+			if len(w) < len(longest) or len(w) > len(s):
+				continue
+
+			i = 0
+			for c in s:
+				if i < len(w) and w[i] == c:
+					i += 1
+
+			if i != len(w):
+				continue
+
+			if len(w) > len(longest) or (len(w) == len(longest) and w < longest):
 				longest = w
 
 		return longest
