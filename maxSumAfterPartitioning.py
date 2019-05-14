341f2302082140cb7872792f8934b1e1fef252c4
Dan <jdanray@users.noreply.github.com>
2019-05-14 13:18:47 -0500

    simplified maxSumAfterPartitioning.py

--- maxSumAfterPartitioning.py
+++ maxSumAfterPartitioning.py
@@ -9,17 +9,14 @@ class Solution(object):
 				return memo[i]
 	
 			m = A[i]	
-			s = A[i]
-			soln = A[i]
+			memo[i] = A[i]
 			for k in range(K):
 				if i + k < len(A):
 					m = max(m, A[i + k])
 					s = m * (k + 1)
 					if i + k + 1 < len(A):
 						s += dp(i + k + 1)
-					soln = max(soln, s)
-
-			memo[i] = soln
+					memo[i] = max(memo[i], s)
 			return memo[i]
 
 		return dp(0)
@@ -30,15 +27,12 @@ class Solution(object):
 		dp = {}
 		for i in range(len(A)):	
 			m = A[i]	
-			s = A[i]
-			soln = A[i]
+			dp[i] = A[i]
 			for k in range(K):
 				if i - k >= 0:
 					m = max(m, A[i - k])
 					s = m * (k + 1)
 					if i - k - 1 >= 0:
 						s += dp[i - k - 1]
-					soln = max(soln, s)
-			dp[i] = soln
-
+					dp[i] = max(dp[i], s)
 		return dp[len(A) - 1]
