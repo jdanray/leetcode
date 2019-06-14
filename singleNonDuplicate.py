0fd35dd16ccefe61cd5a51cabe67137860969395
Dan <jdanray@users.noreply.github.com>
2019-06-13 19:30:13 -0500

    push singleNonDuplicate.py

---
+++ singleNonDuplicate.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+class Solution(object):
+	def singleNonDuplicate(self, nums):
+		lo = 0
+		hi = len(nums) - 1
+		while lo < hi:
+			mid = lo + (hi - lo) // 2
+			if mid % 2 == 1:
+				mid -= 1
+
+			if nums[mid] == nums[mid + 1]:
+				lo = mid + 2
+			else:
+				hi = mid
+
+		return nums[lo]
