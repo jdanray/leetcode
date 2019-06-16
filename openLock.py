94ecd10bf5b3a94a8c0098243e0b2727f6510daf
Dan <jdanray@users.noreply.github.com>
2019-06-16 13:07:12 -0500

    openLock.py: 'lock' var stays a tuple

--- openLock.py
+++ openLock.py
@@ -3,14 +3,14 @@
 class Solution(object):
 	def openLock(self, deadends, target):
 		deadends = {tuple(int(d) for d in dead) for dead in deadends}
-		start = [0, 0, 0, 0]
-		target = [int(t) for t in target]
+		target = tuple(int(t) for t in target)
+		start = (0, 0, 0, 0)
 		seen = set()
 		queue = [[start, 0]]
 		while queue:
 			lock, n = queue.pop(0)
 
-			if tuple(lock) in deadends:
+			if lock in deadends:
 				continue
 
 			if lock == target:
@@ -20,17 +20,17 @@ class Solution(object):
 				up = list(lock)
 				up[i] += 1
 				up[i] %= 10
-				tup = tuple(up)
-				if tup not in seen:
-					seen.add(tup)
+				up = tuple(up)
+				if up not in seen:
+					seen.add(up)
 					queue.append([up, n + 1])
 
 				down = list(lock)
 				down[i] -= 1
 				down[i] %= 10
-				tdown = tuple(down)
-				if tdown not in seen:
-					seen.add(tdown)
+				down = tuple(down)
+				if down not in seen:
+					seen.add(down)
 					queue.append([down, n + 1])
 
 		return -1
