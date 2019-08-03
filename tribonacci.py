d2dc491d7de0748b8b552334381556bf7bd30ee0
Dan <jdanray@users.noreply.github.com>
2019-08-02 22:49:12 -0500

    push tribonacci.py

---
+++ tribonacci.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution(object):
+	def tribonacci(self, n):
+		t = {}
+		t[0] = 0
+		t[1] = 1
+		t[2] = 1
+		for i in range(3, n + 1):
+			t[i] = t[i - 1] + t[i - 2] + t[i - 3]            
+		return t[n]
