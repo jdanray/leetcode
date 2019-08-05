e34410cb4c47513c167a5446ff65d5b9e8a3b8ee
Dan <jdanray@users.noreply.github.com>
2019-08-05 10:08:57 -0500

    movesToMakeZigzag.py: fix URL, tweak comment

--- movesToMakeZigzag.py
+++ movesToMakeZigzag.py
@@ -1,12 +1,12 @@
-# https://leetcode.com/contest/weekly-contest-148/problems/decrease-elements-to-make-array-zigzag/
+# https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/
 
 """
 The strategy is simple:
-nums is a zigzag array if it satisfies one of two conditions. 
-(Let's say that a zigzag array can be a zig array or a zag array.)
+nums is a zigzag array if it satisfies either one of two conditions. 
+Say that a zigzag array can be either a zig array or a zag array.
 So: 
 Try to transform nums into a zig array using only the necessary number of moves
-Try to transform nums ibto a zag array using only the necessary number of moves  
+Try to transform nums into a zag array using only the necessary number of moves  
 Pick the transformation that requires the least number of moves
 """
 
@@ -35,5 +35,4 @@ class Solution(object):
 					d = (zagnums[i+1] - zagnums[i]) + 1
 					zagnums[i+1] -= d
 					zag += d 
-                    
 		return min(zig, zag)
