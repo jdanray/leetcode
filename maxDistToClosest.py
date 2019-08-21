04d5fa903ef74f1704d679047361ab55d0b674d8
Dan <jdanray@users.noreply.github.com>
2019-08-21 03:35:06 -0500

    maxDistToClosest: fix space format

--- maxDistToClosest.py
+++ maxDistToClosest.py
@@ -3,10 +3,7 @@
 class Solution(object):
 	def maxDistToClosest(self, seats):
 		ones = [i for i in range(len(seats)) if seats[i] == 1]
-
 		maxd = max(ones[0], len(seats) - 1 - ones[-1])
-
 		for i in range(1, len(ones)):
 			maxd = max(maxd, (ones[i] - ones[i - 1]) // 2)
-
 		return maxd
