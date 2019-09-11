11cfbf3ee8939105e63c1693e42094535a1c96d4
Dan <jdanray@users.noreply.github.com>
2019-09-11 05:55:31 -0500

    minSubArrayLen.py: added a simpler, faster soln

--- minSubArrayLen.py
+++ minSubArrayLen.py
@@ -2,6 +2,21 @@
 
 class Solution(object):
 	def minSubArrayLen(self, s, nums):
+		i = 0
+		j = 0
+		acc = 0
+		res = float('inf')
+		while j < len(nums):
+			acc += nums[j]
+			while acc >= s:
+				res = min(res, j - i + 1)
+				acc -= nums[i]
+				i += 1
+			j += 1
+		return 0 if res == float('inf') else res
+
+class Solution(object):
+	def minSubArrayLen(self, s, nums):
 		def window(mid):
 			c = 0
 			for i, n in enumerate(nums):
