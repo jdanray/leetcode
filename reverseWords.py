d822808c46d81f4f5ce3378ed643e1e7e3bbdf3e
Dan <jdanray@users.noreply.github.com>
2020-03-28 18:34:51 -0500

    reverseWords.py: change name to reverseWords3.py; push new reverseWords.py

--- reverseWords.py
+++ reverseWords.py
@@ -1,15 +1,16 @@
-# https://leetcode.com/problems/reverse-words-in-a-string-iii/
+# https://leetcode.com/problems/reverse-words-in-a-string/ 
 
-class Solution:
+class Solution(object):
 	def reverseWords(self, s):
-		blank = ' '
-		r = ''
-		w = ''
-		for c in s:
-			if c == blank:
-				r += w
-				r += blank
-				w = ''
-			else:
-				w = c + w
-		return r + w
+		return ' '.join(w for w in reversed(s.split()))
+
+class Solution(object):
+	def reverseWords(self, s):
+		words = s.split()
+		i = 0
+		j = len(words) - 1
+		while i < j:
+			words[i], words[j] = words[j], words[i]
+			i += 1
+			j -= 1
+		return ' '.join(words)
