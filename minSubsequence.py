7088415aca7fc3eb1b4a5144858b7cf3e3c7a93b
Dan <jdanray@users.noreply.github.com>
2020-04-05 22:15:40 -0500

    minSubsequence.py: added O(n) countsort soln

--- minSubsequence.py
+++ minSubsequence.py
@@ -11,3 +11,22 @@ class Solution(object):
 			s += n
 			if s > total - s:
 				return res
+
+class Solution(object):
+	def minSubsequence(self, nums):
+		count = collections.Counter(nums)
+		start = max(count)
+		total = sum(nums)
+		s = 0
+		res = []
+		n = start
+		while n >= 1:
+			while count[n] > 0 and s <= total - s:
+				count[n] -= 1
+				s += n
+				res.append(n)
+
+			if s > total - s:
+				return res
+			else:
+				n -= 1
