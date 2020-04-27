73740639421579623130a5e4b697899f599b9019
Dan <jdanray@users.noreply.github.com>
2020-04-27 16:16:43 -0500

    findMinFibonacciNumbers.py: add iterative soln

--- findMinFibonacciNumbers.py
+++ findMinFibonacciNumbers.py
@@ -16,3 +16,25 @@ class Solution(object):
 				return 1 + helper(k - fib[i], i)
 
 		return helper(k, len(fib) - 1)
+
+"""
+After I solve a problem, I like to study other people's solutions to the problem. The program below is a Python program inspired by the following Java program: 
+
+https://leetcode.com/problems/find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/discuss/585741/Java-Iterative-and-Intuitive-solution
+"""
+
+class Solution(object):
+	def findMinFibonacciNumbers(self, k):
+		a = 1
+		b = 1
+		while b < k:
+			a, b = b, a + b
+
+		res = 0
+		while k > 0: 
+			if b <= k:
+				res += 1
+				k -= b
+			a, b = b - a, a
+
+		return res 
