6b8438d1db3d834e552deec8e7ef7a8a05e03cc9
Dan <jdanray@users.noreply.github.com>
2020-05-18 16:46:42 -0500

    push spiralOrder.py

--- spiralOrder.py
+++ spiralOrder.py
@@ -49,3 +49,39 @@ class Solution(object):
 					colstart += 1
 
 		return res
+
+"""
+After I solve a problem, I like to study other people's solutions. I implemented in Python a nice Java solution which you can find here: https://leetcode.com/problems/spiral-matrix/discuss/20599/Super-Simple-and-Easy-to-Understand-Solution
+"""
+
+class Solution(object):
+	def spiralOrder(self, matrix):
+		if not matrix:
+			return []
+
+		rowstart = 0
+		rowend = len(matrix) - 1
+		colstart = 0
+		colend = len(matrix[0]) - 1
+
+		res = []
+		while rowstart <= rowend and colstart <= colend:
+			for j in range(colstart, colend + 1):
+				res.append(matrix[rowstart][j])
+			rowstart += 1
+
+			for i in range(rowstart, rowend + 1):
+				res.append(matrix[i][colend])
+			colend -= 1
+
+			if rowstart <= rowend:
+				for j in range(colend, colstart - 1, -1):
+					res.append(matrix[rowend][j])
+			rowend -= 1
+
+			if colstart <= colend:
+				for j in range(rowend, rowstart - 1, -1):
+					res.append(matrix[j][colstart])
+			colstart += 1
+
+		return res
