46d8855f96427205492008e7a62c9f20aee23418
Dan <jdanray@users.noreply.github.com>
2020-05-18 02:30:30 -0500

    checkIfExist.py: add soln

--- checkIfExist.py
+++ checkIfExist.py
@@ -7,3 +7,15 @@ class Solution(object):
 				if i != j and num1 * 2 == num2:
 					return True
 		return False
+
+class Solution(object):
+        def checkIfExist(self, arr):
+                double = collections.defaultdict(int)
+                for i, n in enumerate(arr):
+                        double[n * 2] += 1
+
+                for i, n in enumerate(arr):
+                        if n in double and (n != 0 or double[n] > 1):
+                                return True
+
+                return False
