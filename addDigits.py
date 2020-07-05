2a539153caaf1b4599bd7e765f0cd33a718f1402
Dan <jdanray@users.noreply.github.com>
2020-07-04 19:21:18 -0500

    push addDigits.py

---
+++ addDigits.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/add-digits/
+
+class Solution(object):
+	def addDigits(self, num):
+		while num > 9:
+			s = 0
+			while num > 0:
+				s += (num % 10)
+				num //= 10
+			num = s
+		return num
