07595fc34d7b63a4410c85e026b6dde280678dfe
Dan <jdanray@users.noreply.github.com>
2020-08-31 22:15:57 -0500

    lastStoneWeight.py: add new soln

--- lastStoneWeight.py
+++ lastStoneWeight.py
@@ -15,3 +15,16 @@ class Solution:
 			return self.lastStoneWeight(stones)
 
 		return self.lastStoneWeight(stones + [y - x])
+
+class Solution(object):
+	def lastStoneWeight(self, stones):
+		stones = [-s for s in stones]
+		heapq.heapify(stones)
+		while len(stones) >= 2:
+			x = heapq.heappop(stones)
+			y = heapq.heappop(stones)
+
+			if x != y:
+				heapq.heappush(stones, x - y)
+
+		return abs(stones[0]) if stones else 0
