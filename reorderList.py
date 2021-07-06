a6a82a909a3aeb3ac93f97753ef230b9a7aab672
Dan <jdanray@users.noreply.github.com>
2021-07-06 17:53:25 -0500

    reorderList.py: added new soln

--- reorderList.py
+++ reorderList.py
@@ -22,3 +22,19 @@ class Solution(object):
 
 			i += 1
 			j -= 1
+
+# I revisited this old problem and came up with a more elegant solution: 
+
+class Solution(object):
+	def reorderList(self, head):
+		lst = []
+		while head:
+			lst.append(head)
+			head = head.next
+
+		N = len(lst)
+		for i in range(N // 2):
+			lst[i].next = lst[N - i - 1]
+			lst[N - i - 1].next = lst[i + 1]
+
+		lst[N // 2].next = None
