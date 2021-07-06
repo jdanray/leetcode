79af7f05d8b3c2ef19e6c1b3888114d5406b23e2
Dan <jdanray@users.noreply.github.com>
2021-07-06 18:33:02 -0500

    mergeTwoLists.py: added new soln

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -28,3 +28,18 @@ class Solution(object):
 
 		node.next = l1 if l1 else l2
 		return head.next
+
+# I revisited this old problem and came up with a much more elegant solution:
+
+class Solution(object):
+	def mergeTwoLists(self, l1, l2):
+		if not l1:
+			return l2        
+		elif not l2:
+			return l1        
+		elif l1.val < l2.val:
+			l1.next = self.mergeTwoLists(l1.next, l2)
+			return l1
+		else:
+			l2.next = self.mergeTwoLists(l1, l2.next)
+			return l2
