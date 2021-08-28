c9a42a9a0f2ceb206c81b74aa918262efa478375
Dan <jdanray@users.noreply.github.com>
2021-08-27 19:52:42 -0500

    maximumUnits.py: add soln

--- maximumUnits.py
+++ maximumUnits.py
@@ -11,3 +11,18 @@ class Solution(object):
 			if truckSize == 0: 
 				break
 		return res
+
+class Solution(object):
+	def maximumUnits(self, boxTypes, truckSize):
+		pq = [(-u, b) for (b, u) in boxTypes]
+		heapq.heapify(pq)
+
+		res = 0
+		while truckSize > 0 and pq:
+			u, b = heapq.heappop(pq)
+
+			m = min(b, truckSize)
+			res -= (u * m)
+			truckSize -= m
+
+		return res
