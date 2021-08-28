89d2e92ef10fa915fa32dd3afc36a76fe0658433
Dan <jdanray@users.noreply.github.com>
2021-08-28 01:25:06 -0500

    partitionLabels.py: added simpler soln

--- partitionLabels.py
+++ partitionLabels.py
@@ -22,3 +22,23 @@ class Solution(object):
 			i = j + 1
 
 		return nlabels
+
+class Solution(object):
+	def partitionLabels(self, s):
+		maxLoc = collections.defaultdict(int)
+		for i, c in enumerate(s):
+			maxLoc[c] = i
+
+		start = 0
+		res = []
+		while start < len(s):
+			i = start
+			m = maxLoc[s[start]]
+			while i < m:
+				m = max(m, maxLoc[s[i]])
+				i += 1
+
+			res.append(i - start + 1)
+			start = i + 1
+
+		return res
