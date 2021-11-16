a2d803b214223de530a8562822b7c37aad3322b8
Dan <jdanray@users.noreply.github.com>
2021-11-16 03:20:00 -0600

    maxFreq.py: added sliding window soln

--- maxFreq.py
+++ maxFreq.py
@@ -23,3 +23,28 @@ class Solution(object):
 				chars.add(s[j])
 
 		return max(count.values()) if count else 0
+
+class Solution(object):
+	def maxFreq(self, s, maxLetters, minSize, maxSize):
+		i = 0
+		res = 0
+		uniques = 0
+		seenChars = collections.Counter()
+		countSubs = collections.Counter()
+		for j, c in enumerate(s):
+			if seenChars[c] == 0:
+				uniques += 1
+			seenChars[c] += 1
+
+			while uniques > maxLetters or j - i + 1 > minSize:
+				seenChars[s[i]] -= 1
+				if seenChars[s[i]] == 0:
+					uniques -= 1
+				i += 1
+
+			if j - i + 1 == minSize:
+				sub = s[i:j + 1]
+				countSubs[sub] += 1
+				res = max(res, countSubs[sub])
+
+		return res
