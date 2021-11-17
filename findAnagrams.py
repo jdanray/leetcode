c6e39121534af01160c6d511de4e90dd99f27168
Dan <jdanray@users.noreply.github.com>
2021-11-16 20:14:22 -0600

    findAnagrams.py: added sliding window soln

--- findAnagrams.py
+++ findAnagrams.py
@@ -22,3 +22,23 @@ class Solution:
 				del scount[s[i]]
 
 		return indices
+
+class Solution(object):
+	def findAnagrams(self, s, p):
+		countAnagram = collections.Counter(p)
+		countChars = collections.Counter()
+		i = 0
+		res = []
+		for j, c in enumerate(s):
+			countChars[c] += 1
+
+			while any(countChars[x] > countAnagram[x] for x in countChars):
+				countChars[s[i]] -= 1
+				if countChars[s[i]] == 0: 
+					del countChars[s[i]]
+				i += 1
+
+			if countChars == countAnagram:
+				res.append(i)
+
+		return res
