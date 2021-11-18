f68971c422434a932be9a7c0e9158c5ccbd67fb6
Dan <jdanray@users.noreply.github.com>
2021-11-17 18:04:53 -0600

    countGoodSubstrings.py: added sliding window soln

--- countGoodSubstrings.py
+++ countGoodSubstrings.py
@@ -7,3 +7,23 @@ class Solution(object):
 			if s[i] != s[i - 1] and s[i] != s[i - 2] and s[i - 1] != s[i - 2]:
 				res += 1
 		return res
+
+class Solution(object):
+	def countGoodSubstrings(self, s):
+		def atMost(s, limit):
+			i = 0
+			res = 0
+			count = collections.Counter()
+			for j, c in enumerate(s):
+				count[c] += 1
+
+				while count[c] > 1 or j - i + 1 > limit:
+					count[s[i]] -= 1
+					i += 1
+
+				res += j - i + 1
+
+			return res
+
+		limit = 3
+		return atMost(s, limit) - atMost(s, limit - 1)
