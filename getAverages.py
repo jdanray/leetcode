7baa3bb0e2008fba96b204bfa2949a0fb82b1739
Dan <jdanray@users.noreply.github.com>
2021-11-28 14:17:59 -0600

    getAverages.py: added prefix sum soln

--- getAverages.py
+++ getAverages.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/k-radius-subarray-averages/ 
 
+# sliding-window solution
+
 class Solution(object):
 	def getAverages(self, nums, k):
 		N = len(nums)
@@ -13,5 +15,22 @@ class Solution(object):
 			if i >= k2:
 				res[i - k] = s / (k2 + 1)
 				s -= nums[i - k2]
+
+		return res
+
+# prefix-sum solution
+
+class Solution(object):
+	def getAverages(self, nums, k):
+		N = len(nums)
+		subarrSize = 2 * k + 1
+
+		prefixSum = [0 for _ in range(N + 1)]
+		for i in range(N):
+			prefixSum[i + 1] = prefixSum[i] + nums[i]
+
+		res = [-1 for _ in range(N)]
+		for j in range(k, N - k):
+			res[j] = (prefixSum[j + k + 1] - prefixSum[j - k]) / subarrSize
 
 		return res
