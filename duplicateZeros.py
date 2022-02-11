43a4097b3de84ca6cf041f296f4630041996395b
Dan <jdanray@users.noreply.github.com>
2022-02-11 16:25:24 -0600

    duplicateZeros.py: added linear-time solns

--- duplicateZeros.py
+++ duplicateZeros.py
@@ -3,6 +3,34 @@
 class Solution(object):
 	def duplicateZeros(self, arr):
 		N = len(arr)
+		queue = collections.deque(arr)
+		i = 0
+		while i < N:
+			n = queue.popleft()
+
+			arr[i] = n
+			i += 1
+
+			if n == 0 and i < N:
+				arr[i] = 0
+				i += 1
+
+
+class Solution(object):
+	def duplicateZeros(self, arr):
+		N = len(arr)
+		queue = collections.deque(arr)
+		i = 0
+		while i < N:
+			n = queue.popleft()
+            
+			for _ in range(1 + (n == 0 and i + 1 < N)):
+				arr[i] = n
+				i += 1
+
+class Solution(object):
+	def duplicateZeros(self, arr):
+		N = len(arr)
 		i = 0
 		while i < N:
 			if arr[i] != 0 or N <= i + 1:
