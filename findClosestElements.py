b28e7d41707d1b60c86e6faf330bef4ae80633cc
Dan <jdanray@users.noreply.github.com>
2022-09-29 00:16:02 -0500

    findClosestElements.py: provided new solns

--- findClosestElements.py
+++ findClosestElements.py
@@ -1,34 +1,60 @@
 # https://leetcode.com/problems/find-k-closest-elements/
 
-class Solution:
+class Solution(object):
 	def findClosestElements(self, arr, k, x):
-		c = self.closest(arr, x)
-		i = c - 1
-		j = c + 1
-		res = [arr[c]]
-		while len(res) < k:
-			if i >= 0 and (j >= len(arr) or abs(arr[i] - x) <= abs(arr[j] - x)):
-				res = [arr[i]] + res
-				i -= 1
-			else:
-				res += [arr[j]]
-				j += 1
-		return res
+		m = float('inf')
+		j = 0
+		s = 0
+		for i, a in enumerate(arr):
+			s += abs(a - x)
 
-	def closest(self, arr, x):
-		lo = 0
-		hi = len(arr) - 1
-		c = 0
-		while lo <= hi:
-			mid = lo + (hi - lo) // 2
-			if arr[mid] == x:
-				return mid
+			if i >= k:
+				s -= abs(arr[i - k] - x)
 
-			if abs(arr[mid] - x) < abs(arr[c] - x):
-				c = mid
-		
-			if arr[mid] < x:
-				lo = mid + 1
+			if i >= k - 1 and s < m:
+				j = i
+				m = s
+
+		return arr[j - k + 1:j + 1]
+
+"""
+After I solve a problem, I like to study other people's solutions. I found two other solutions that I wanted to save.
+
+The first solution comes from here: https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
+
+It is a binary search for the first index i s.t. arr[i] is closer to x than arr[i + k] is. The code here is slightly different from the Python code found at the link. I have a binary search template, and I modified the code to fit the template.
+
+The second solution comes from here: https://leetcode.com/problems/find-k-closest-elements/discuss/202785/Very-simple-Java-O(n)-solution-using-two-pointers
+
+It is a two-pointers solution. Like the binary-search solution, it takes advantage of the fact that arr is sorted. I translated into Python the Java code found at the link.
+"""
+
+class Solution(object):
+	def findClosestElements(self, arr, k, x):
+		def condition(value):
+			return x - arr[value] <= arr[value + k] - x
+
+		left = 0
+		right = len(arr) - k
+		while left < right:
+			mid = left + (right - left) // 2
+
+			if condition(mid):
+				right = mid
 			else:
-				hi = mid - 1                
-		return c	
+				left = mid + 1
+
+		return arr[left:left + k]
+
+
+class Solution(object):
+	def findClosestElements(self, arr, k, x):
+		i = 0
+		j = len(arr) - 1
+		while j - i >= k:
+			if abs(arr[i] - x) > abs(arr[j] - x):
+				i += 1
+			else:
+				j -= 1
+
+		return arr[i:j + 1]
