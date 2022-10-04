217c55865be0e8444ce152ef75b77255d5996079
Dan <jdanray@users.noreply.github.com>
2022-10-03 19:30:14 -0500

    hasPathSum.py: added recursive soln

--- hasPathSum.py
+++ hasPathSum.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/path-sum/description
+# https://leetcode.com/problems/path-sum/
 
 class Solution(object):
 	def hasPathSum(self, root, target):
@@ -17,3 +17,13 @@ class Solution(object):
 			if node.right:
 				stack.append([node.right, psum + node.right.val])
 		return False
+
+class Solution(object):
+	def hasPathSum(self, root, sum):
+		if not root:
+			return False
+
+		if not root.left and not root.right:
+			return root.val == sum
+
+		return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
