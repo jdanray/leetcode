831cc1eb0e89b5d36d1202c2ee33751d6be8b922
Dan <jdanray@users.noreply.github.com>
2022-11-07 20:51:53 -0600

    makeGood.py: simplified

--- makeGood.py
+++ makeGood.py
@@ -2,17 +2,13 @@
 
 class Solution(object):
 	def makeGood(self, s):
-		N = len(string.ascii_lowercase)
-		lo2up = {string.ascii_lowercase[i]: string.ascii_uppercase[i] for i in range(N)}
-		up2lo = {string.ascii_uppercase[i]: string.ascii_lowercase[i] for i in range(N)}
-
 		stack = []
 		for c in s:
 			if not stack:
 				stack.append(c)
-			elif c.isupper() and up2lo[c] == stack[-1]:
+			elif c.isupper() and c.lower() == stack[-1]:
 				stack.pop()
-			elif c.islower() and lo2up[c] == stack[-1]:
+			elif c.islower() and c.upper() == stack[-1]:
 				stack.pop()
 			else:
 				stack.append(c)
