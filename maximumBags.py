bb0de2c025902049aef5f6b7f795e4a366a9e38c
Dan <jdanray@users.noreply.github.com>
2022-12-26 19:38:20 -0600

    maximumBags.py: added non-heap soln

--- maximumBags.py
+++ maximumBags.py
@@ -19,3 +19,19 @@ class Solution(object):
 				res += 1
 
 		return res
+
+class Solution(object):
+	def maximumBags(self, capacity, rocks, additionalRocks):
+		needed = [capacity[i] - rocks[i] for i in range(len(capacity))]
+		needed = sorted(needed)
+
+		res = 0
+		for n in needed:
+			if additionalRocks >= n:
+				additionalRocks -= n
+				n = 0
+
+			if n == 0:
+				res += 1
+
+		return res
