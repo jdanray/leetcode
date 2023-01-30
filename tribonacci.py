4e3ae430a2988cca51bab35061a46f7aafa1e96f
Dan <jdanray@users.noreply.github.com>
2023-01-29 18:45:53 -0600

    tribonacci.py: added O(1) space soln

--- tribonacci.py
+++ tribonacci.py
@@ -9,3 +9,11 @@ class Solution(object):
 		for i in range(3, n + 1):
 			t[i] = t[i - 1] + t[i - 2] + t[i - 3]            
 		return t[n]
+
+class Solution(object):
+	def tribonacci(self, n):
+		t = [0, 1, 1]
+		for _ in range(3, n + 1):
+			t = [t[1], t[2], t[0] + t[1] + t[2]]
+
+		return t[n] if n < len(t) else t[-1]
