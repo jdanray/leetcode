e270b77fcc797990205efb41f9845f515b54e81e
Dan <jdanray@users.noreply.github.com>
2023-02-10 18:47:48 -0600

    shortestAlternatingPaths.py: more simplifying

--- shortestAlternatingPaths.py
+++ shortestAlternatingPaths.py
@@ -1,35 +1,35 @@
 # https://leetcode.com/problems/shortest-path-with-alternating-colors/ 
 
 class Solution(object):
-	def shortestAlternatingPaths(self, n, red_edges, blue_edges):
+	def shortestAlternatingPaths(self, n, redEdges, blueEdges):
 		graphRed = collections.defaultdict(set)
-		for u, v in red_edges:
+		for (u, v) in redEdges:
 			graphRed[u].add(v)
 
 		graphBlue = collections.defaultdict(set)
-		for u, v in blue_edges:
+		for (u, v) in blueEdges:
 			graphBlue[u].add(v)
 
 		seenRed = set()
 		seenBlue = set()
-		queue = [[0, -1, 0]]
+		queue = collections.deque([[0, -1, 0]])
 		res = [-1] * n
 		while queue:
-			vertex, prev, plen = queue.pop(0)
+			u, prev, plen = queue.popleft()
 
-			if res[vertex] == -1:
-				res[vertex] = plen
+			if res[u] == -1:
+				res[u] = plen
 
 			if prev == 0 or prev == -1:
-				for u in graphBlue[vertex]:
+				for v in graphBlue[u]:
 					if (u, v) not in seenBlue:
-						queue.append([u, 1, plen + 1])
+						queue.append([v, 1, plen + 1])
 						seenBlue.add((u, v))
 
 			if prev == 1 or prev == -1:
-				for u in graphRed[vertex]:
+				for v in graphRed[u]:
 					if (u, v) not in seenRed:
-						queue.append([u, 0, plen + 1])
+						queue.append([v, 0, plen + 1])
 						seenRed.add((u, v))
 			
 		return res
