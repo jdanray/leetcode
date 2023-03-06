dae49740d9e16b9015aa368f5e702d13c44feca9
Dan <jdanray@users.noreply.github.com>
2023-03-05 19:17:43 -0600

    push findKthPositive.py

--- findKthPositive.py
+++ findKthPositive.py
@@ -14,3 +14,21 @@ class Solution(object):
 			m += 1
 
 		return arr[-1] + k
+
+"""
+The following program is simpler than the above program. 
+
+However, there is an issue with the following program: The loop doesn't make progress toward termination at each iteration.
+
+But the loop is guaranteed to terminate: n increases at each iteration, and arr must have finite length. So, eventually n will be greater than the greatest element of arr, and then n won't be in arr. Then k will surely decrease until it reaches 0, and then the loop terminates.
+"""
+
+class Solution(object):
+	def findKthPositive(self, arr, k):
+		arr = set(arr)
+		n = 0
+		while k > 0:
+			n += 1
+			if n not in arr:
+				k -= 1
+		return n 
