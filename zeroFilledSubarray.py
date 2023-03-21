bcb7cc9b84610e1c92221d147f193ac73d0bed67
Dan <jdanray@users.noreply.github.com>
2023-03-20 19:07:32 -0500

    zeroFilledSubarray.py: added slightly simpler soln

--- zeroFilledSubarray.py
+++ zeroFilledSubarray.py
@@ -10,3 +10,14 @@ class Solution(object):
 					i = j
 				res += (j - i + 1)
 		return res
+
+class Solution(object):
+	def zeroFilledSubarray(self, nums):
+		i = 0
+		res = 0
+		for j, n in enumerate(nums):
+			if n == 0:
+				res += j - i + 1
+			else:
+				i = j + 1
+		return res
