bdf12a7343d1181fbce68ca74e48b91939dff64a
Dan <jdanray@users.noreply.github.com>
2023-04-17 19:10:11 -0500

    mergeAlternately.py: simplify soln

--- mergeAlternately.py
+++ mergeAlternately.py
@@ -4,19 +4,17 @@ class Solution(object):
 	def mergeAlternately(self, word1, word2):
 		M = len(word1)
 		N = len(word2)
+
 		i = 0
 		j = 0
-		res = ""
+		res = ''
 		while i < M or j < N:
-			if i < M and j < N:
-				res += word1[i]
-				res += word2[j]
-				i += 1
-				j += 1
-			elif i < M:
+			if i < M:
 				res += word1[i]
 				i += 1
-			else:
+
+			if j < N:
 				res += word2[j]
 				j += 1
+
 		return res
