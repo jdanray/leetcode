1d25bf19010959bb4f9a443c8457552c29a34206
Dan <jdanray@users.noreply.github.com>
2023-04-23 19:16:24 -0500

    lastStoneWeight.py: deleted inefficient soln, prettied up efficient soln

--- lastStoneWeight.py
+++ lastStoneWeight.py
@@ -1,30 +1,15 @@
 # https://leetcode.com/problems/last-stone-weight/
 
-class Solution:
-	def lastStoneWeight(self, stones):
-		if not stones:
-			return 0
-
-		stones = sorted(stones)
-		y = stones.pop()
-		if not stones:
-			return y
-
-		x = stones.pop()
-		if x == y:
-			return self.lastStoneWeight(stones)
-
-		return self.lastStoneWeight(stones + [y - x])
-
 class Solution(object):
 	def lastStoneWeight(self, stones):
-		stones = [-s for s in stones]
-		heapq.heapify(stones)
-		while len(stones) >= 2:
-			x = heapq.heappop(stones)
-			y = heapq.heappop(stones)
+		pq = [-s for s in stones]
+		heapq.heapify(pq)
 
-			if x != y:
-				heapq.heappush(stones, x - y)
+		while len(pq) > 1:
+			u = heapq.heappop(pq)
+			v = heapq.heappop(pq)
 
-		return abs(stones[0]) if stones else 0
+			if u != v:
+				heapq.heappush(pq, u - v)
+
+		return abs(pq[0]) if pq else 0
