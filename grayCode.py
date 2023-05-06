f26c73d0fc9709697b075c1f748dabb074add19a
Dan <jdanray@users.noreply.github.com>
2023-05-06 17:04:27 -0500

    grayCode.py: added iterative soln

--- grayCode.py
+++ grayCode.py
@@ -10,3 +10,11 @@ class Solution(object):
 			res.append(num + (1 << (n - 1))) 
             
 		return res
+
+class Solution(object):
+	def grayCode(self, n):
+		res = [0]
+		for i in range(1, n + 1):
+			for num in res[::-1]:
+				res.append(num + (1 << (i - 1))) 
+		return res
