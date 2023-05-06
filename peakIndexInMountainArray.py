2c434a9d66596e7ae9c8b5c9156849426051b0f0
Dan <jdanray@users.noreply.github.com>
2023-05-06 16:42:35 -0500

    peakIndexInMountainArray.py: added binary search template soln

--- peakIndexInMountainArray.py
+++ peakIndexInMountainArray.py
@@ -1,11 +1,21 @@
 # https://leetcode.com/problems/peak-index-in-a-mountain-array/
 
 class Solution(object):
-	def peakIndexInMountainArray(self, A):
-		i = 0
-		while A[i] < A[i + 1]:
-			i += 1
-		return i
+	def peakIndexInMountainArray(self, arr):
+		def condition(value):
+			return arr[value] > arr[value + 1]
+
+		left = 1
+		right = len(arr) - 2
+		while left < right:
+			mid = left + (right - left) // 2
+
+			if condition(mid):
+				right = mid
+			else:
+				left = mid + 1
+
+		return left
 
 class Solution(object):
 	def peakIndexInMountainArray(self, A):
@@ -19,3 +29,10 @@ class Solution(object):
 				lo = mid
 			else:
 				return mid
+
+class Solution(object):
+	def peakIndexInMountainArray(self, A):
+		i = 0
+		while A[i] < A[i + 1]:
+			i += 1
+		return i
