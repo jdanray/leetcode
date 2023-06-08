ea2f219c64f045e150b6c963bd0f73add50a79ba
Dan <jdanray@users.noreply.github.com>
2023-06-07 19:28:15 -0500

    countNegatives.py: add O(M + N) soln and 1-liner O(M*N) soln

--- countNegatives.py
+++ countNegatives.py
@@ -1,5 +1,27 @@
 # https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/ 
 
+# O(M + N)
+class Solution(object):
+	def countNegatives(self, grid):
+		M = len(grid)
+		N = len(grid[0])
+
+		j = 0
+		res = 0
+		for i in range(M - 1, -1, -1):
+			while j < N and grid[i][j] >= 0:
+				j += 1
+
+			res += N - j
+
+		return res
+
+# O(M * N)
+class Solution(object):
+	def countNegatives(self, grid):
+		return sum(grid[i][j] < 0 for i in range(len(grid)) for j in range(len(grid[0])))
+
+# O(M * N)
 class Solution(object):
 	def countNegatives(self, grid):
 		n = 0
