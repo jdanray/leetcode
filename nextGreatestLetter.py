0447144318448d8f4dda933408b37b6ed7316438
Dan <jdanray@users.noreply.github.com>
2023-06-08 21:22:10 -0500

    nextGreatestLetter.py: add simpler O(N)-time soln, add binary-search soln

--- nextGreatestLetter.py
+++ nextGreatestLetter.py
@@ -9,3 +9,28 @@ class Solution(object):
 		while target >= letters[i]:
 			i += 1
 		return letters[j]
+
+class Solution(object):
+	def nextGreatestLetter(self, letters, target):
+		for l in letters:
+			if l > target:
+				return l
+            
+		return letters[0]
+
+class Solution(object):
+	def nextGreatestLetter(self, letters, target):
+		def condition(value):
+			return letters[value] > target
+
+		left = 0
+		right = len(letters)
+		while left < right:
+			mid = left + (right - left) // 2
+
+			if condition(mid):
+				right = mid
+			else:
+				left = mid + 1
+
+		return letters[left % len(letters)]
