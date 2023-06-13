acf27e4e6907ab85dad95d0c680f9ad26b0a102e
Dan <jdanray@users.noreply.github.com>
2023-06-12 20:53:32 -0500

    equalPairs.py: add quadratic-time soln

--- equalPairs.py
+++ equalPairs.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/equal-row-and-column-pairs/ 
 
+# Time: O(N**3)
+# Space: O(1)
 class Solution(object):
 	def equalPairs(self, grid):
 		N = len(grid)
@@ -13,5 +15,23 @@ class Solution(object):
 
 				if k == N:
 					res += 1
+
+		return res
+
+# Time: O(N**2)
+# Space: O(N**2)
+class Solution(object):
+	def equalPairs(self, grid):
+		N = len(grid)
+
+		count = collections.Counter()
+		for j in range(N):
+			col = tuple(grid[i][j] for i in range(N))
+			count[col] += 1
+
+		res = 0
+		for i in range(N):
+			row = tuple(grid[i][j] for j in range(N))
+			res += count[row]
 
 		return res
