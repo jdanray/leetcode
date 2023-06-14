3707476d01a93c3e7319e196955dc845d4876f91
Dan <jdanray@users.noreply.github.com>
2023-06-13 22:45:57 -0500

    getMinimumDifference.py: added three O(n)-time inorder-traversal solns

--- getMinimumDifference.py
+++ getMinimumDifference.py
@@ -21,3 +21,58 @@ class Solution(object):
 			res = min(res, nums[i] - nums[i - 1])
 
 		return res
+
+class Solution(object):
+	def getMinimumDifference(self, root):
+		def inOrder(root):
+			return inOrder(root.left) + [root.val] + inOrder(root.right) if root else []
+
+		nums = inOrder(root)
+		res = float('inf')
+		for i in range(1, len(nums)):
+			res = min(res, nums[i] - nums[i - 1])
+
+		return res
+
+class Solution(object):
+	def getMinimumDifference(self, root):
+		self.res = float('inf')
+
+		def inOrder(root):
+			if not root:
+				return []
+
+			left = inOrder(root.left)
+			right = inOrder(root.right)
+
+			if left:
+				self.res = min(self.res, root.val - left[-1]) 
+
+			if right:
+				self.res = min(self.res, right[0] - root.val)
+
+			return left + [root.val] + right
+
+		inOrder(root)
+		return self.res
+
+class Solution(object):
+	def getMinimumDifference(self, root):
+		self.prev = None
+		self.res = float('inf')
+        
+		def inOrder(root):
+			if not root:
+				return
+
+			inOrder(root.left)
+
+			if self.prev:
+				self.res = min(self.res, root.val - self.prev.val)
+
+			self.prev = root
+
+			inOrder(root.right)
+
+		inOrder(root)
+		return self.res
