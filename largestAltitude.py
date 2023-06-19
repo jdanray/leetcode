5541804bdfecadfbf98aab650f9716d23434d8b0
Dan <jdanray@users.noreply.github.com>
2023-06-18 19:41:40 -0500

    largestAltitude.py: tidied up original soln; added 1-liner soln

--- largestAltitude.py
+++ largestAltitude.py
@@ -2,9 +2,13 @@
 
 class Solution(object):
 	def largestAltitude(self, gain):
+		alt = 0
 		res = 0
-		alt = 0		
 		for g in gain:
 			alt += g
 			res = max(res, alt)
 		return res
+
+class Solution(object):
+	def largestAltitude(self, gain):
+		return max(0, max(itertools.accumulate(gain)))
