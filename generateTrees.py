6d02f1681bb468257e5cfb9b41fe12c24b9e316a
Dan <jdanray@users.noreply.github.com>
2023-08-04 20:16:41 -0500

    push generateTrees.py

---
+++ generateTrees.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/unique-binary-search-trees-ii/ 
+
+class Solution(object):
+	def generateTrees(self, n):
+		def generate(l, r):
+			if l > r:
+				return [None]
+			elif l == r:
+				return [TreeNode(l)]
+
+			res = []
+			for val in range(l, r + 1):
+				for left in generate(l, val - 1):
+					for right in generate(val + 1, r):
+						node = TreeNode(val)
+						node.left = left
+						node.right = right
+						res.append(node)
+
+			return res 
+
+		return generate(1, n)
