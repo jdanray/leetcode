24b678b8e5b62ec058bc25a9ea8689bf1fe05b6d
Dan <jdanray@users.noreply.github.com>
2023-08-12 19:20:22 -0500

    validPartition.py: add soln with less cluttered code

--- validPartition.py
+++ validPartition.py
@@ -1,6 +1,35 @@
 # https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/ 
 
 # Use python3 to memoize the backtracking
+
+class Solution:
+	def validPartition(self, nums):
+		N = len(nums)
+
+		@functools.lru_cache(None)
+		def dp(i):
+			if i >= N:
+				return True
+
+			if i + 1 >= N:
+				return False
+
+			if nums[i] == nums[i + 1] and dp(i + 2):
+				return True
+
+			if i + 2 >= N:
+				return False
+
+			if nums[i] == nums[i + 1] and nums[i] == nums[i + 2] and dp(i + 3):
+				return True
+
+			if nums[i] == nums[i + 1] - 1 and nums[i] == nums[i + 2] - 2 and dp(i + 3):
+				return True
+
+			return False
+
+		return dp(0) 
+
 class Solution(object):
 	def validPartition(self, nums):
 		N = len(nums)
