558246342b64422053bf4a7f18fca384a3d3dd58
Dan <jdanray@users.noreply.github.com>
2023-08-13 19:24:52 -0500

    findKthLargest.py: add O(n)-time soln

--- findKthLargest.py
+++ findKthLargest.py
@@ -5,7 +5,7 @@ Complexity:
 
 Time: If pushing a number onto PQ causes PQ's size to become greater than K, then we pop the smallest element off PQ. So, because the size of PQ will not stay greater than K, pushing and popping is O(lg(K)). And, because we push each of the N numbers, the time-complexity is O(N * log(K)). However, K == N in the worst case.
 
-Space: We create one data structure (ie, PQ), and PQ may not contain more than K elements. So, the space-complexity is O(K). 
+Space: We create one data structure (ie, PQ), and PQ may not contain more than K elements. So, the space-complexity is O(K). However, K == N in the worst case. 
 """
 
 class Solution(object):
@@ -13,6 +13,23 @@ class Solution(object):
 		pq = []
 		for n in nums:
 			heapq.heappush(pq, n)
+
 			if len(pq) > k:
 				heapq.heappop(pq)
+
 		return pq[0]
+
+"""
+Complexity:
+Time: O(N)
+Space: O(N)
+"""
+
+class Solution(object):
+	def findKthLargest(self, nums, k):
+		count = collections.Counter(nums)
+		for n in range(max(nums), min(nums) - 1, -1):
+			if count[n] < k:
+				k -= count[n]
+			else:
+				return n
