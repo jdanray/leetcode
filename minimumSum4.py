5bcdd556a9bfb6fb1afd5e8adb66d3ed12c628af
Dan <jdanray@users.noreply.github.com>
2023-10-23 05:55:56 -0500

    push minimumSum3.py, minimumSum4.py

---
+++ minimumSum4.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/minimum-sum-of-mountain-triplets-ii/ 
+
+class Solution(object):
+	def minimumSum(self, nums):
+		N = len(nums)
+
+		minLeft = [float('inf') for _ in range(N)]
+		for i in range(1, N):
+			minLeft[i] = min(minLeft[i - 1], nums[i - 1])
+
+		minRight = nums[-1]
+		res = float('inf')
+		for j in range(N - 2, 0, -1):
+			if minLeft[j] < nums[j] and minRight < nums[j]:
+				res = min(res, minLeft[j] + nums[j] + minRight)
+
+			minRight = min(minRight, nums[j])
+
+		return -1 if res == float('inf') else res
