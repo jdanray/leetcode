46e3b5dba216a30c920a00b96cec673ab74d7e33
Dan <jdanray@users.noreply.github.com>
2023-10-27 03:13:57 -0500

    canPartition.py: add top-down soln

--- canPartition.py
+++ canPartition.py
@@ -16,3 +16,20 @@ class Solution:
 				can[s] |= can[s - n]
 
 		return can[total]
+
+class Solution(object):
+	def canPartition(self, nums):
+		sumNums = sum(nums)
+
+		memo = {}
+		def dp(i, s):
+			if i >= len(nums):
+				return s == sumNums - s
+
+			if (i, s) in memo:
+				return memo[i, s]
+
+			memo[i, s] = dp(i + 1, s + nums[i]) or dp(i + 1, s)
+			return memo[i, s]
+
+		return dp(0, 0)
