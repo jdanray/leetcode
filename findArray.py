f67c9f61ccbb93826b7841bfefa7144e39c37340
Dan <jdanray@users.noreply.github.com>
2023-10-30 19:19:52 -0500

    findArray.py: simplify soln

--- findArray.py
+++ findArray.py
@@ -2,11 +2,11 @@
 
 class Solution(object):
 	def findArray(self, pref):
-		res = [pref[0]]
-		xor = pref[0]
-		for i in range(1, len(pref)):
-			r = xor ^ pref[i]
-			res.append(r)
+		xor = 0
+		res = []
+		for p in pref:
+			r = xor ^ p
 			xor ^= r
-            
+			res.append(r)
+
 		return res
