2b8e43021c60e32a3d7d560abdbb6db06ba65a72
Dan <jdanray@users.noreply.github.com>
2023-11-02 19:26:59 -0500

    buildArray.py: add soln

--- buildArray.py
+++ buildArray.py
@@ -2,6 +2,21 @@
 
 class Solution(object):
 	def buildArray(self, target, n):
+		k = 1
+		res = []
+		for t in target:
+			while k < t:
+				res.append("Push")
+				res.append("Pop")
+				k += 1
+
+			res.append("Push")
+			k += 1
+
+		return res
+
+class Solution(object):
+	def buildArray(self, target, n):
 		last = target[-1]
 		target = set(target)
 		res = []
