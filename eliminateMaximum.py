c1bb91fe2c15ce8bf55143724b565e24c19d6a4d
Dan <jdanray@users.noreply.github.com>
2023-11-06 18:25:35 -0600

    eliminateMaximum.py: add soln

--- eliminateMaximum.py
+++ eliminateMaximum.py
@@ -2,6 +2,14 @@
 
 class Solution(object):
 	def eliminateMaximum(self, dist, speed):
+		t = sorted(float(d) / speed[i] for i, d in enumerate(dist))
+		i = 1
+		while i < len(t) and t[i] > i:
+			i += 1
+		return i
+
+class Solution(object):
+	def eliminateMaximum(self, dist, speed):
 		N = len(dist)        
 		minutes = sorted((dist[i] - 1) // speed[i] for i in range(N))
 		for i, m in enumerate(minutes):
