1dcee451b3ea123da619ded3779ee20e9712dd70
Dan <jdanray@users.noreply.github.com>
2023-11-07 20:28:39 -0600

    connect.py: add new soln

--- connect.py
+++ connect.py
@@ -2,6 +2,26 @@
 
 class Solution(object):
 	def connect(self, root):
+		queue = [root]
+		while queue:
+			prev = None
+			for _ in range(len(queue)):
+				node = queue.pop(0)
+
+				if not node:
+					continue
+
+				if prev != None:
+					prev.right = node
+
+				prev = node
+				queue.append(node.left)
+				queue.append(node.right)
+
+		return root
+
+class Solution(object):
+	def connect(self, root):
 		front = None
 		curlvl = 0
 		queue = [[0, root]]
