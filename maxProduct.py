13ae1ecfa2bfbed23ae8506620afd6677bf7e5f2
Dan <jdanray@users.noreply.github.com>
2023-12-11 18:34:18 -0600

    push maxProduct.py

--- maxProduct.py
+++ maxProduct.py
@@ -2,5 +2,17 @@
 
 class Solution(object):
 	def maxProduct(self, nums):
+		m1 = 0
+		m2 = 0
+		for n in nums:
+			if n > m1:
+				m1, m2 = n, m1
+			elif n > m2:
+				m2 = n
+
+		return (m1 - 1) * (m2 - 1)
+
+class Solution(object):
+	def maxProduct(self, nums):
 		nums = sorted(nums)
 		return (nums[-1] - 1) * (nums[-2] - 1)
