e15105d92e40fda5e6e29a779ebc98c67890bc18
Dan <jdanray@users.noreply.github.com>
2023-12-12 19:08:49 -0600

    numSpecial.py: add quadratic-time soln

--- numSpecial.py
+++ numSpecial.py
@@ -20,3 +20,24 @@ class Solution(object):
 				res += 1
 
 		return res
+
+class Solution(object):
+	def numSpecial(self, mat):
+		M = len(mat)
+		N = len(mat[0])
+
+		row = {}
+		for i in range(M):
+			row[i] = sum(mat[i])
+
+		col = {}
+		for j in range(N):
+			col[j] = sum(mat[k][j] for k in range(M))
+
+		res = 0
+		for i in range(M):
+			for j in range(N):
+				if mat[i][j] == 1 and row[i] == 1 and col[j] == 1:
+					res += 1
+
+		return res
