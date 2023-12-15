9c7f34d29f2be6fd1b83aa038d225e0bb8fafdca
Dan <jdanray@users.noreply.github.com>
2023-12-14 18:46:51 -0600

    destCity.py: add three new solns

--- destCity.py
+++ destCity.py
@@ -10,3 +10,33 @@ class Solution(object):
 		outgoing = set(u for u, _ in paths)
 
 		return list(cities - outgoing)[0]
+
+class Solution(object):
+	def destCity(self, paths):
+		graph = collections.defaultdict(str)
+		for (u, v) in paths:
+			graph[u] = v
+
+		u = paths[0][0]
+		while u in graph:
+			u = graph[u]
+
+		return u
+
+class Solution(object):
+	def destCity(self, paths):
+		cands = collections.defaultdict(bool)
+		for (a, b) in paths:
+			cands[a] = False
+			if b not in cands:
+				cands[b] = True
+
+		for c in cands:
+			if cands[c]:
+				return c
+
+class Solution(object):
+	def destCity(self, paths):
+		for (_, b) in paths:
+			if not any(b == c for (c, _) in paths):
+				return b
