bbbd47f53e92fcea5b525c53d33b2f6e01087632
Dan <jdanray@users.noreply.github.com>
2024-01-31 18:46:28 -0600

    divideArray2.py: add two solns

--- divideArray2.py
+++ divideArray2.py
@@ -2,6 +2,28 @@
 
 class Solution(object):
 	def divideArray(self, nums, k):
+		N = 3
+
+		if len(nums) % N != 0:
+			return []
+
+		heapq.heapify(nums)
+		res = []
+		while len(nums) >= N:
+			abc = []
+			for _ in range(N):
+				n = heapq.heappop(nums)
+				abc.append(n)
+
+			if abc[-1] - abc[0] > k:
+				return []
+
+			res.append(abc)
+
+		return res
+
+class Solution(object):
+	def divideArray(self, nums, k):
 		if len(nums) % 3 != 0:
 			return []
 
@@ -18,3 +40,14 @@ class Solution(object):
 			res.append([a, b, c])
 
 		return res
+
+class Solution(object):
+	def divideArray(self, nums, k):
+		nums = sorted(nums)
+		res = []
+		for i in range(0, len(nums), 3):
+			if nums[i + 2] - nums[i] > k:
+				return []
+			else:
+				res.append([nums[i], nums[i + 1], nums[i + 2])
+		return res 
