fb5b8e64fa40e3ef0fab9a9c0aaaa4976948c3e0
Dan <jdanray@users.noreply.github.com>
2024-03-12 21:48:46 -0500

    pivotInteger.py: add soln

--- pivotInteger.py
+++ pivotInteger.py
@@ -2,6 +2,20 @@
 
 class Solution(object):
 	def pivotInteger(self, n):
+		tot = n * (n + 1) // 2
+		s = 0
+		for u in range(1, n + 1):
+			s += u
+            
+			if s == tot:
+				return u
+            
+			tot -= u
+
+		return -1
+
+class Solution(object):
+	def pivotInteger(self, n):
 		pre = collections.defaultdict(int)
 		s = 0
 		for i in range(1, n + 1):
