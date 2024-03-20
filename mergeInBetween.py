be3e8d56255a5061ee467b3725bbbf71feb2b7fe
Dan <jdanray@users.noreply.github.com>
2024-03-20 03:51:32 -0500

    mergeInBetween.py: add soln

--- mergeInBetween.py
+++ mergeInBetween.py
@@ -19,3 +19,38 @@ class Solution(object):
 		stop1.next = list2
 		tail2.next = start1
 		return list1
+
+class Solution(object):
+	def mergeInBetween(self, list1, a, b, list2):
+		start = None	# the list1 node after which list2 starts
+		tail = None	# the remaining "tail" of list1 that will be added to the end of list2
+		i = 0
+		node = list1
+		while node:
+			if i == a - 1:
+				start = node
+
+			if i == b + 1:
+				tail = node
+
+			node = node.next
+			i += 1
+
+		# if !start, then a == 0
+		# we cannot return list1 as head
+		if start:
+			head = list1
+			start.next = list2
+		else:
+			head = list2
+
+		# find the end of list2
+		# append the "tail" of list1 to the end of list2
+		list2end = None
+		node = list2
+		while node:
+			list2end = node
+			node = node.next
+		list2end.next = tail
+
+		return head
