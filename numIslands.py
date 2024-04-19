640659e7e9ea37a055b328abde3c42d055ada5b1
Dan <jdanray@users.noreply.github.com>
2024-04-18 21:38:23 -0500

    numIslands.py: add two new solns

--- numIslands.py
+++ numIslands.py
@@ -24,3 +24,54 @@ class Solution:
 							stack.append((m, n))
 
 		return nislands
+
+# Modify grid directly to keep track of visited lands
+class Solution(object):
+	def numIslands(self, grid):
+		M = len(grid)
+		N = len(grid[0])
+		deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+		def mark(i, j):
+			grid[i][j] = '0'
+
+			for (di, dj) in deltas:
+				ni = i + di
+				nj = j + dj
+				if ni >= 0 and ni < M and nj >= 0 and nj < N and grid[ni][nj] == '1':
+					mark(ni, nj)
+
+		res = 0
+		for i in range(M):
+			for j in range(N):
+				if grid[i][j] == '1':
+					res += 1
+					mark(i, j)
+
+		return res
+
+# Store coordinates of visited lands in a set
+class Solution(object):
+	def numIslands(self, grid):
+		M = len(grid)
+		N = len(grid[0])
+		deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+		seen = set()
+		def mark(i, j):
+			seen.add((i, j))
+
+			for (di, dj) in deltas:
+				ni = i + di
+				nj = j + dj
+				if (ni, nj) not in seen and ni >= 0 and ni < M and nj >= 0 and nj < N and grid[ni][nj] == '1':
+					mark(ni, nj)
+
+		res = 0
+		for i in range(M):
+			for j in range(N):
+				if (i, j) not in seen and grid[i][j] == '1':
+					res += 1
+					mark(i, j)
+
+		return res
