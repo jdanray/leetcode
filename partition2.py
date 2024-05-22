a0a5b31cd3f7281eaed937b0ef92a57cf21a2477
Dan <jdanray@users.noreply.github.com>
2024-05-21 19:12:37 -0500

    partition2.py: define all functions inside partition()

--- partition2.py
+++ partition2.py
@@ -1,28 +1,28 @@
 # https://leetcode.com/problems/palindrome-partitioning/ 
 
 class Solution(object):
-	def isPalindrome(self, p):
-		i = 0
-		j = len(p) - 1
-		while i < j:
-			if p[i] != p[j]:
-				return False
-			i += 1
-			j -= 1
-		return True
-
-	def helper(self, s, start, part):
-		if start >= len(s):
-			self.res.append(part)
+	def partition(self, s):
+		def isPalindrome(p):
+			i = 0
+			j = len(p) - 1
+			while i < j:
+				if p[i] != p[j]:
+					return False
+				i += 1
+				j -= 1
 			return True
 
-		p = ''
-		for i in range(start, len(s)):
-			p += s[i]
-			if self.isPalindrome(p):
-				self.helper(s, i + 1, part + [p])
+		def doPartition(start, part):
+			if start >= len(s):
+				res.append(part)
+				return True
 
-	def partition(self, s):
-		self.res = []
-		self.helper(s, 0, [])
-		return self.res
+			p = ''
+			for i in range(start, len(s)):
+				p += s[i]
+				if isPalindrome(p):
+					doPartition(i + 1, part + [p])
+
+		res = []
+		doPartition(0, [])
+		return res
