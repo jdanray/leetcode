d0a38f42915aa5be109a7887c64a504b77c66075
Dan <jdanray@users.noreply.github.com>
2024-05-24 19:27:11 -0500

    wordBreak.py: add bottom-up DP soln

--- wordBreak.py
+++ wordBreak.py
@@ -24,3 +24,19 @@ class Solution(object):
 			return memo[i]
 
 		return dp(0)
+
+class Solution(object):
+	def wordBreak(self, s, wordDict):
+		N = len(s)
+		wordDict = set(wordDict)
+
+		dp = [False for _ in range(N + 1)]
+		dp[0] = True
+		for i in range(1, N + 1):
+			w = ''
+			for j in range(i, N + 1):
+				w += s[j - 1]
+				if w in wordDict and dp[i - 1]:
+					dp[j] = True
+
+		return dp[-1]
