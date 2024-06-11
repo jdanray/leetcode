efb4eae72fc536949e974be1bf3990d31af11cd9
Dan <jdanray@users.noreply.github.com>
2024-06-10 19:34:44 -0500

    relativeSortArray.py: add two solns

--- relativeSortArray.py
+++ relativeSortArray.py
@@ -15,3 +15,30 @@ class Solution(object):
 				rest.append(n)
 
 		return ans + rest
+
+class Solution(object):
+	def relativeSortArray(self, arr1, arr2):
+		MAX = 1000
+
+		count = collections.Counter(arr1)
+		res = []
+		for n2 in arr2:
+			while count[n2] > 0:
+				res.append(n2)
+				count[n2] -= 1
+
+		for n1 in range(MAX + 1):
+			while count[n1] > 0:
+				res.append(n1)
+				count[n1] -= 1
+
+		return res
+
+class Solution(object):
+	def relativeSortArray(self, arr1, arr2):
+		idx = {a: i for i, a in enumerate(arr2)}
+        
+		left = [a for a in arr1 if a in idx]
+		right = [a for a in arr1 if a not in idx]
+        
+		return sorted(left, key=lambda a: idx[a]) + sorted(right)
