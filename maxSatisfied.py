bc5b5928a2cb2e627e0d75c2e0741f2de41e6a6d
Dan <jdanray@users.noreply.github.com>
2024-06-20 20:42:48 -0500

    maxSatisfied.py: add slightly simpler soln

--- maxSatisfied.py
+++ maxSatisfied.py
@@ -16,3 +16,18 @@ class Solution(object):
 			i += 1
 
 		return M
+
+class Solution(object):
+	def maxSatisfied(self, customers, grumpy, minutes):
+		s = sum(c for i, c in enumerate(customers) if grumpy[i] == 0)
+		res = s
+		for i in range(len(customers)):
+			if grumpy[i] == 1:
+				s += customers[i]
+
+			if i >= minutes and grumpy[i - minutes] == 1:
+				s -= customers[i - minutes]
+
+			res = max(res, s)
+
+		return res
