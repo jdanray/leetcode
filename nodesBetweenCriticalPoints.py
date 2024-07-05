1e1744c8cf5f0af7af56c483ff3f5bf649e2da47
Dan <jdanray@users.noreply.github.com>
2024-07-04 19:52:10 -0500

    nodesBetweenCriticalPoints.py: add soln

--- nodesBetweenCriticalPoints.py
+++ nodesBetweenCriticalPoints.py
@@ -2,6 +2,31 @@
 
 class Solution(object):
 	def nodesBetweenCriticalPoints(self, head):
+		firstCrit = -1
+		prevCrit = -1
+		prev = None
+		i = 0
+		res = [-1, -1]
+		while head and head.next:
+			if prev and (prev.val < head.val > head.next.val or prev.val > head.val < head.next.val):
+				if prevCrit != -1 and (res[0] == -1 or i - prevCrit < res[0]):
+					res[0] = i - prevCrit
+
+				if firstCrit != -1:
+					res[1] = i - firstCrit
+
+				prevCrit = i
+				if firstCrit == -1:
+					firstCrit = i
+
+			prev = head
+			head = head.next
+			i += 1
+
+		return res
+
+class Solution(object):
+	def nodesBetweenCriticalPoints(self, head):
 		preVal = -1
 		firstCrit = -1
 		preCrit = -1
