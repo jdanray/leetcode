c24169bbcfda736e17c035f9b62c089e307d22b1
Dan <jdanray@users.noreply.github.com>
2024-07-08 19:55:12 -0500

    averageWaitingTime.py: add slightly simpler soln

--- averageWaitingTime.py
+++ averageWaitingTime.py
@@ -2,6 +2,16 @@
 
 class Solution(object):
 	def averageWaitingTime(self, customers):
+		t = 0
+		wait = 0.0
+		for (a, p) in customers:
+			t = max(t, a) + p
+			wait += t - a
+
+		return wait / len(customers)
+
+class Solution(object):
+	def averageWaitingTime(self, customers):
 		start = 1
 		wait = 0.0
 		for (a, p) in customers:
