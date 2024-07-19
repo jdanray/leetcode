3e1c8377df660ab08cb069b9d192ba5a793ab2cf
Dan <jdanray@users.noreply.github.com>
2024-07-18 20:11:36 -0500

    luckyNumbers.py: add new soln, pretty up old soln

--- luckyNumbers.py
+++ luckyNumbers.py
@@ -4,6 +4,22 @@ class Solution(object):
 	def luckyNumbers(self, matrix):
 		M = len(matrix)
 		N = len(matrix[0])
-		colmaxs = set(max(matrix[i][j] for i in range(M)) for j in range(N))
-		rowmins = set(min(row) for row in matrix)
-		return list(colmaxs & rowmins)
+
+		minim = set(min(matrix[i][j] for j in range(N)) for i in range(M))
+		maxim = set(max(matrix[i][j] for i in range(M)) for j in range(N))
+
+		return list(minim & maxim)
+
+class Solution(object):
+	def luckyNumbers(self, matrix):
+		M = len(matrix)
+		N = len(matrix[0])
+
+		res = []
+		for i in range(M):
+			minJ = min(list(range(N)), key=lambda j: matrix[i][j])
+			maxi = max(matrix[k][minJ] for k in range(M))
+			if maxi == matrix[i][minJ]:
+				res.append(maxi)
+
+		return res
