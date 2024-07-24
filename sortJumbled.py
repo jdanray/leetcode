0478fb4f34b43de0781b8842d4272e815263c895
Dan <jdanray@users.noreply.github.com>
2024-07-23 19:39:37 -0500

    sortJumbled.py: add somewhat different implementation

--- sortJumbled.py
+++ sortJumbled.py
@@ -2,6 +2,25 @@
 
 class Solution(object):
 	def sortJumbled(self, mapping, nums):
+		vals = []
+		for i, n in enumerate(nums):
+			if n == 0:
+				vals.append((mapping[0], i))
+				continue
+
+			p = 0
+			m = 0
+			while n > 0:
+				m += mapping[n % 10] * 10 ** p
+				p += 1
+				n //= 10
+
+			vals.append((m, i))
+
+		return [nums[i] for (_, i) in sorted(vals)]
+
+class Solution(object):
+	def sortJumbled(self, mapping, nums):
 		def convert(num):
 			if num == 0:
 				return mapping[0]
