ea21c27c276dbd10ba783042fa280cf1779f36cd
Dan <jdanray@users.noreply.github.com>
2024-08-08 23:35:20 -0500

    numMagicSquaresInside.py: add more generalized soln

--- numMagicSquaresInside.py
+++ numMagicSquaresInside.py
@@ -26,3 +26,49 @@ class Solution(object):
 					res += 1
 
 		return res
+
+class Solution(object):
+	def numMagicSquaresInside(self, grid):
+		K = 3
+		M = len(grid)
+		N = len(grid[0])
+
+		res = 0
+		for i in range(M - 2):
+			for j in range(N - 2):
+				nums = {grid[i + r][j + c] for r in range(K) for c in range(K)}
+				if nums != set(range(1, K * K + 1)):
+					continue
+
+				s = sum(grid[i][j + l] for l in range(K))
+				fail = False
+
+				for k in range(K):
+					r = sum(grid[i + k][j + l] for l in range(K))
+					if r != s:
+						fail = True
+						break
+
+				if fail:
+					continue
+
+				for l in range(K):
+					c = sum(grid[i + k][j + l] for k in range(K))
+					if c != s:
+						fail = True
+						break
+
+				if fail:
+					continue
+
+				d1 = sum(grid[i + k][j + k] for k in range(K))
+				if d1 != s:
+					continue
+
+				d2 = sum(grid[i + k][j + K - 1 - k] for k in range(K))
+				if d2 != s:
+					continue
+
+				res += 1
+
+		return res
