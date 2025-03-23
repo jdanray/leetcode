e0f06ac256f249825ccd259e98a99e0e4e4e0a9d
Daniel Ray <jdanray@users.noreply.github.com>
2025-03-23 01:17:26 -0500

    mergeArrays.py: add soln

--- mergeArrays.py
+++ mergeArrays.py
@@ -13,3 +13,35 @@ class Solution(object):
 			sums[i] += v
 
 		return [[i, sums[i]] for i in range(1, M + 1) if sums[i] > 0]
+
+class Solution(object):
+	def mergeArrays(self, nums1, nums2):
+		M = len(nums1)
+		N = len(nums2)
+
+		i = 0
+		j = 0
+		res = []
+		while i < M or j < N:
+			while i < M and j < N and nums1[i][0] < nums2[j][0]:
+				res.append(nums1[i])
+				i += 1
+
+			while i < M and j < N and nums2[j][0] < nums1[i][0]:
+				res.append(nums2[j])
+				j += 1
+
+			while i < M and j < N and nums1[i][0] == nums2[j][0]:
+				res.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+				i += 1
+				j += 1
+
+			while i < M and j >= N:
+				res.append(nums1[i])
+				i += 1
+
+			while i >= M and j < N:
+				res.append(nums2[j])
+				j += 1
+
+		return res
