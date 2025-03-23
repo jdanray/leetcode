53c137fc40ee1ef4c254f437695f4d16f89f2d02
Daniel Ray <jdanray@users.noreply.github.com>
2025-03-23 01:26:06 -0500

    smallestNumber2.py: add BFS soln

--- smallestNumber2.py
+++ smallestNumber2.py
@@ -17,3 +17,20 @@ class Solution(object):
 			return res
 
 		return min(backtrack(''))
+
+class Solution(object):
+	def smallestNumber(self, pattern):
+		digits = list(range(1, 10))
+
+		queue = [[[], set()]]
+		while queue:
+			num, used = queue.pop(0)
+
+			if len(num) == len(pattern) + 1:
+				return ''.join(str(d) for d in num)
+
+			for d in digits:
+				if d not in used and (not num or (pattern[len(num) - 1] == 'I' and d > num[-1]) or (pattern[len(num) - 1] == 'D' and d < num[-1])):
+					queue.append([num + [d], used | {d}])
+
+		return ''
