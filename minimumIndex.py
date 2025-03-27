a0a2098fa01369dfa57f4da1b705fe91d007818b
Daniel Ray <jdanray@users.noreply.github.com>
2025-03-26 20:07:36 -0500

    minimumIndex.py: add slightly different implementation

--- minimumIndex.py
+++ minimumIndex.py
@@ -27,3 +27,34 @@ class Solution(object):
 				return i
 
 		return -1
+
+class Solution(object):
+	def minimumIndex(self, nums):
+		# Boyer-Moore majority voting algorithm
+		# Find the dominant element
+		c = 0
+		dom = 0
+		for n in nums:
+			if c == 0:
+				dom = n
+
+			if n == dom:
+				c += 1
+			else:
+				c -= 1
+
+		count = 0
+		for n in nums:
+			if n == dom:
+				count += 1
+
+		left = 0
+		for i, n in enumerate(nums):
+			if n == dom:
+				left += 1
+			right = count - left
+            
+			if left * 2 > i + 1 and right * 2 > len(nums) - i - 1:
+				return i
+
+		return -1
