b2dea49dd45a47e1e7e1346068f6e8b1fb474837
Daniel Ray <jdanray@users.noreply.github.com>
2025-04-04 19:09:50 -0500

    subsetXORSum.py: add iterative soln

--- subsetXORSum.py
+++ subsetXORSum.py
@@ -8,3 +8,18 @@ class Solution(object):
 			else:
 				return allxors(i + 1, sub) + allxors(i + 1, sub ^ nums[i])
 		return allxors(0, 0)
+
+class Solution(object):
+	def subsetXORSum(self, nums):
+		stack = [[0, 0]]
+		res = 0
+		while stack:
+			x, i = stack.pop()
+
+			if i >= len(nums):
+				res += x
+			else:
+				stack.append([x ^ nums[i], i + 1])
+				stack.append([x, i + 1])
+
+		return res
