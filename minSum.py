e17cfe94b479fa6bc48f143598634f695608bd48
Daniel Ray <jdanray@users.noreply.github.com>
2025-05-09 19:33:38 -0500

    minSum.py: no need for .count(0); just test if 0 in nums

--- minSum.py
+++ minSum.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/
+#
 
 class Solution(object):
 	def minSum(self, nums1, nums2):
@@ -8,6 +8,6 @@ class Solution(object):
 		if s1 == s2:
 			return s1
 		elif s1 > s2:
-			return s1 if nums2.count(0) > 0 else -1
+			return s1 if 0 in nums2 else -1
 		elif s2 > s1:
-			return s2 if nums1.count(0) > 0 else -1
+			return s2 if 0 in nums1 else -1
