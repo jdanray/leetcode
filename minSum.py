bac02c6f616ba86df018245efe196ff2e16d46c9
Daniel Ray <jdanray@users.noreply.github.com>
2025-05-09 19:29:36 -0500

    push minSum.py

---
+++ minSum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/
+
+class Solution(object):
+	def minSum(self, nums1, nums2):
+		s1 = sum(1 if n == 0 else n for n in nums1)
+		s2 = sum(1 if n == 0 else n for n in nums2)
+
+		if s1 == s2:
+			return s1
+		elif s1 > s2:
+			return s1 if nums2.count(0) > 0 else -1
+		elif s2 > s1:
+			return s2 if nums1.count(0) > 0 else -1
