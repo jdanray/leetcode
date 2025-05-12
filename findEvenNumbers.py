bc86b383a4b7f64874affec8a1265eac5d6b9a46
Daniel Ray <jdanray@users.noreply.github.com>
2025-05-11 19:51:15 -0500

    findEvenNumbers.py: add backtrack soln

--- findEvenNumbers.py
+++ findEvenNumbers.py
@@ -18,3 +18,19 @@ class Solution(object):
 					res.add(100*c + 10*b + a)
 
 		return sorted(res)
+
+class Solution(object):
+	def findEvenNumbers(self, digits):
+		nums = set()
+		def build(n):
+			if len(n) == 3:
+				r = digits[n[0]] + (digits[n[1]] * 10) + (digits[n[2]] * 100)
+				nums.add(r)
+				return True
+
+			for i, d in enumerate(digits):
+				if i not in n and (n or d % 2 == 0) and (len(n) != 2 or d != 0):
+					build(n + [i])
+
+		build([])
+		return sorted(nums)
