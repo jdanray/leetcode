c8be5b25ff77af892149da8af0d0160417eb77d9
Daniel Ray <jdanray@users.noreply.github.com>
2025-05-20 19:28:46 -0500

    setZeroes.py: add two new solns

--- setZeroes.py
+++ setZeroes.py
@@ -1,6 +1,53 @@
 # https://leetcode.com/problems/set-matrix-zeroes/description/
 
-class Solution:
+# Time: O(M * N)
+# Space: O(M + N)
+class Solution(object):
+	def setZeroes(self, matrix):
+		M = len(matrix)
+		N = len(matrix[0])
+
+		row = collections.defaultdict(bool)
+		col = collections.defaultdict(bool)
+		for i in range(M):
+			for j in range(N):
+				if matrix[i][j] == 0:
+					row[i] = True
+					col[j] = True
+
+		for i in range(M):
+			for j in range(N):
+				if row[i] or col[j]:
+					matrix[i][j] = 0
+
+# Time: O(M * N * max(M, N))
+# Space: O(1)
+class Solution(object):
+	def setZeroes(self, matrix):
+		M = len(matrix)
+		N = len(matrix[0])
+
+		for i in range(M):
+			for j in range(N):
+				if matrix[i][j] != 0:
+					continue
+
+				for k in range(M):
+					if matrix[k][j] != 0:
+						matrix[k][j] = float('inf')
+
+				for k in range(N):
+					if matrix[i][k] != 0:
+						matrix[i][k] = float('inf')
+
+		for i in range(M):
+			for j in range(N):
+				if matrix[i][j] == float('inf'):
+					matrix[i][j] = 0
+
+# Time: O(M * N * max(M, N))
+# Space: O(M * N)
+class Solution(object):
 	def setZeroes(self, matrix):
 		changed = set()
 		for i in range(len(matrix)):
@@ -15,8 +62,8 @@ class Solution:
 							matrix[k][j] = 0
 							changed.add((k, j))
 
-# I revisited this old problem and came up with an O(1)-space solution:
-
+# Time: O(M * N * max(M, N))
+# Space: O(1)
 class Solution(object):
 	def setZeroes(self, matrix):
 		INF = 2**32
