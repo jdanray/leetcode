ae3e65dd1078b4f18ad9a8647a2cf1a2c6343ffd
Daniel Ray <jdanray@users.noreply.github.com>
2025-07-14 19:14:22 -0500

    isValid2.py: add soln

--- isValid2.py
+++ isValid2.py
@@ -6,3 +6,25 @@ class Solution(object):
 		consonants = set(string.ascii_lowercase + string.ascii_uppercase) - set(vowels)
 
 		return len(word) >= 3 and word.isalnum() and any(v in word for v in vowels) and any(c in word for c in consonants)
+
+class Solution(object):
+	def isValid(self, word):
+		if len(word) < 3:
+			return False
+
+		vowels = set('aeiouAEIOU')
+		cons = set(string.ascii_lowercase + string.ascii_uppercase) - vowels
+
+		hasVowel = False
+		hasCons = False
+		for c in word:
+			if not c.isalnum():
+				return False
+
+			if c in vowels:
+				hasVowel = True
+
+			if c in cons:
+				hasCons = True
+
+		return hasVowel and hasCons
