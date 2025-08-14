0a2ee2ebe531f98ef6368f64483ec15ac606323e
Daniel Ray <jdanray@users.noreply.github.com>
2025-08-13 19:27:13 -0500

    largestGoodInteger.py: add simpler general soln

--- largestGoodInteger.py
+++ largestGoodInteger.py
@@ -3,6 +3,26 @@
 class Solution(object):
 	def largestGoodInteger(self, num):
 		L = 3
+
+		sub = 1
+		prev = ''
+		res = ''
+		for n in num:
+			if n == prev:
+				sub += 1
+			else:
+				sub = 1
+
+			if sub >= L and n > res:
+				res = n
+
+			prev = n
+
+		return res * L
+
+class Solution(object):
+	def largestGoodInteger(self, num):
+		L = 3
 		i = 0
 		res = ''
 		for j in range(len(num)):
