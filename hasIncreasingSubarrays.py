3d6c2835b7436760860af6a86f5f68f76e2e4a85
Daniel Ray <jdanray@users.noreply.github.com>
2025-10-13 19:26:45 -0500

    hasIncreasingSubarrays.py: add O(1)-space soln

--- hasIncreasingSubarrays.py
+++ hasIncreasingSubarrays.py
@@ -2,6 +2,22 @@
 
 class Solution(object):
 	def hasIncreasingSubarrays(self, nums, k):
+		prev = 0
+		inc = 1
+		for i in range(1, len(nums)):
+			if nums[i] > nums[i - 1]:
+				inc += 1
+			else:
+				prev = inc
+				inc = 1
+
+			if inc == 2 * k or (inc == k and prev >= k): 
+				return True
+
+		return False
+
+class Solution(object):
+	def hasIncreasingSubarrays(self, nums, k):
 		inc = [1 for _ in range(len(nums))]
 		for i, n in enumerate(nums):
 			if i - 1 >= 0 and n > nums[i - 1]:
