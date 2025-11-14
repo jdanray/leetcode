660664e7ed7e756a84a3b9bc253cf2f977cd4b37
Daniel Ray <jdanray@users.noreply.github.com>
2025-11-14 00:02:22 -0600

    push rangeAddQueries.py

---
+++ rangeAddQueries.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/increment-submatrices-by-one/
+
+class Solution(object):
+	def rangeAddQueries(self, n, queries):
+		matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+		for (row1, col1, row2, col2) in queries:
+			for i in range(row1, row2 + 1):
+				matrix[i][col1] += 1
+				if col2 + 1 < n:
+					matrix[i][col2 + 1] -= 1
+
+		for i in range(n):
+			for j in range(1, n):
+				matrix[i][j] += matrix[i][j - 1]
+
+		return matrix
