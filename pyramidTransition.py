2fa544ca98d83bc1caf7ef289997a5f0b53663ec
Daniel Ray <jdanray@users.noreply.github.com>
2025-12-28 21:04:11 -0600

    pyramidTransition.py: add soln

--- pyramidTransition.py
+++ pyramidTransition.py
@@ -1,5 +1,37 @@
 # https://leetcode.com/problems/pyramid-transition-matrix/ 
 
+class Solution(object):
+	def pyramidTransition(self, bottom, allowed):
+		triangle = collections.defaultdict(set)
+		for a in allowed:
+			triangle[a[0] + a[1]].add(a[2])
+
+		memo = {}
+		def check(bottom):
+			if len(bottom) == 1:
+				return True
+
+			if bottom in memo:
+				return memo[bottom]
+
+			tops = ['']
+			for i in range(len(bottom) - 1):
+				p = bottom[i] + bottom[i + 1]
+
+				if p not in triangle:
+					return False
+
+				newTops = []
+				for r in triangle[p]:
+					for t in tops:
+						newTops.append(t + r)
+				tops = newTops
+
+			memo[bottom] = any(check(t) for t in tops)
+			return memo[bottom]
+
+		return check(bottom)
+
 class Solution:
 	def allperms(self, blocks, p='', i=0):
 		if i >= len(blocks):
