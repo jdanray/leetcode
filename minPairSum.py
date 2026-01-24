e935e5e7d578a672f3c7450eaac8e6908699738a
Daniel Ray <jdanray@users.noreply.github.com>
2026-01-23 20:27:37 -0600

    minPairSum.py: add correctness argument

--- minPairSum.py
+++ minPairSum.py
@@ -1,5 +1,9 @@
 # https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
 
+"""
+Initially, we haven't selected/paired any numbers. Now, let nums[j] be the largest number that we haven't selected yet. If nums[j] is a part of the minimized maximum pair (nums[i], nums[j]), then nums[i] must be the smallest available number; otherwise, nums[i] + nums[j] won't be minimal. Therefore, the largest unpaired number must always be paired with the smallest unpaired number.
+"""
+
 class Solution(object):
 	def minPairSum(self, nums):
 		nums = sorted(nums)
